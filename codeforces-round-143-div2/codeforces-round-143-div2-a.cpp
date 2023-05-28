14ec976c8e4f2cb68ddd04328f339cd947062cb6
Jorge Ktch <jorgektch@gmail.com>
2023-05-27 19:39:28 -0500

    .

---
+++ codeforces-round-143-div2/codeforces-round-143-div2-a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+	int n; cin >> n;
+	int cont = 0;
+	for(int i=0; i<n; i++){
+		int a, b, c;
+		cin >> a >> b >> c;
+		if(a+b+c>=2) cont++;
+	}
+	cout << cont;
+	return 0;
+}
