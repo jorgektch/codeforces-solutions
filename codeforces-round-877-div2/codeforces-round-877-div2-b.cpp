d839a0904777911bb092700223066d6dc5ef8617
Jorge Ktch <jorgektch@gmail.com>
2023-06-04 08:55:42 -0500

    .

---
+++ codeforces-round-877-div2/codeforces-round-877-div2-b.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int t; cin >> t;
+    for(int ti=0; ti<t; ti++){
+        int n, k; cin >> n >> k;
+        vector<int> v(n);
+        for(int i=1; i<n; i++){ cin >> v[i];
+        
+        cout << cont << endl;
+    }
+    return 0;
+}
