9cf7e28390e552d5c21b0b4bc1865a3a52bef2aa
Jorge Ktch <jorgektch@gmail.com>
2023-08-31 18:20:37 -0500

    New

--- educational-codeforces-round-154-div2/educational-codeforces-round-154-div2-b.cpp
+++ educational-codeforces-round-154-div2/educational-codeforces-round-154-div2-b.cpp
@@ -1,31 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-string equalsString(string cad1, string cad2){
-    string cad = "";
-    for(int i=0; i<cad1.size(); i++){
-        if(cad1[i] == cad2[i]){
-            cad += cad1[i];
-        }else{
-            cad += ' ';
-        }
-    }
-    return cad;
-}
-string check(string cad1, string cad2){
-    string cad = equalsString(cad1, cad2);
-    vector<int> ones;
-    vector<int> zeros;
-    for(int i=0; i<cad.size(); i++){
-        if(cad[i] == '1'){
-            ones.push_back(cad[i]);
-        }else if(cad[i] == '0'){
-            zeros.push_back(cad[i]);
-        }
-    }
-    // PENDIENTE
-
-    return "YES";
-}
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
@@ -33,7 +7,13 @@ int main(){
     for(int i=0; i<n; i++){
         string s1, s2;
         cin >> s1 >> s2;
-        cout << check(s1, s2) << endl;
+        string res = "NO";
+        for(int j=0; j<s1.size()-1; j++){
+            if(s1[j]=='0' && s1[j+1]=='1' && s2[j]=='0' && s2[j+1]=='1'){
+                res = "YES";
+            }
+        }
+        cout << res << endl;
     }
     return 0;
 }
