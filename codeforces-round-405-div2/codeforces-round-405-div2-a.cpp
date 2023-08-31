78b20f2289abecca5bc30ead21e8b7ce3eba319c
Jorge Ktch <jorgektch@gmail.com>
2023-08-31 11:44:20 -0500

    New

--- codeforces-round-405-div2/codeforces-round-405-div2-a.cpp
+++ codeforces-round-405-div2/codeforces-round-405-div2-a.cpp
@@ -3,17 +3,13 @@ using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int n, k; cin >> n >> k;
-    vector<int> arr(n);
-    for(int i=0; i<n; i++){
-        cin >> arr[i];
+    int a, b; cin >> a >> b;
+    int n = 0;
+    while(!(a > b)){
+        a = 3*a;
+        b = 2*b;
+        n++;
     }
-    int count = 0;
-    for(int i=0; i<n; i++){
-        if(arr[i]>0 && arr[i]>=arr[k-1]){
-            count++;
-        }
-    }
-    cout << count << endl;
+    cout << n << endl;
     return 0;
 }
