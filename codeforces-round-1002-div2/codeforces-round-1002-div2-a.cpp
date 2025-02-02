bad4167a8ca69d61c9d992c451e9c107d1a96463
Jorge Ktch <jorgektch@gmail.com>
2025-02-02 01:07:58 -0500

    Update File

--- codeforces-round-1002-div2/codeforces-round-1002-div2-a.cpp
+++ codeforces-round-1002-div2/codeforces-round-1002-div2-a.cpp
@@ -4,8 +4,8 @@ int main(){
     int t; cin >> t;
     while(t--){
         int n; cin >> n;
-        for(int i=0; i<n, i++){
-            
+        for(int i=0; i<n; i++){
+            cout << "Hola" << endl;
         }
     }
     return 0;
