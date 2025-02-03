a3eb26ada928146ba2991f9c4762b11f03f0a884
Jorge Ktch <jorgektch@gmail.com>
2025-02-02 22:28:22 -0500

    Update file

---
+++ codeforces-round-0971-div4/a.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve(){
+    int a, b; cin >> a >> b;
+    cout << b-a << endl;
+}
+int main(){
+    int t; cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
